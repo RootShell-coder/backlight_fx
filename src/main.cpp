19134b4f4e3c17330c15b95a56e60076c24273b0
RootShell-coder <Root.Shelling@gmail.com>
2023-11-07 14:37:39 +0300

    ref

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,35 @@
 #include <Homie.h>
 #include "ambilight.h"
 
-
 #define BRAND "Ambilight"
 #define FW_NAME "illuminated"
 #define FW_VER "1.2.1"
 
-void onHomieEvent(const HomieEvent &event){
-  switch (event.type){
-    case HomieEventType::WIFI_CONNECTED:
-      Serial << "Wi-Fi connected: " << (int8_t)event.wifiReason << endl;
+void onHomieEvent(const HomieEvent &event)
+{
+  switch (event.type)
+  {
+  case HomieEventType::WIFI_CONNECTED:
+    Serial << "Wi-Fi connected: " << (int8_t)event.wifiReason << endl;
     break;
-    case HomieEventType::MQTT_READY:
-      Serial << "MQTT connected: " << (int8_t)event.mqttReason << endl;
+  case HomieEventType::MQTT_READY:
+    Serial << "MQTT connected: " << (int8_t)event.mqttReason << endl;
     break;
-    case HomieEventType::WIFI_DISCONNECTED:
-      Serial << "Wi-Fi disconnected, reason: " << (int8_t)event.wifiReason << endl;
+  case HomieEventType::WIFI_DISCONNECTED:
+    Serial << "Wi-Fi disconnected, reason: " << (int8_t)event.wifiReason << endl;
     break;
-    case HomieEventType::MQTT_DISCONNECTED:
-      Serial << "MQTT disconected, reason: " << (int8_t)event.mqttReason << endl;
+  case HomieEventType::MQTT_DISCONNECTED:
+    Serial << "MQTT disconected, reason: " << (int8_t)event.mqttReason << endl;
     break;
-    default:
+  default:
     break;
   }
 }
 
 ambilightNode AmbilightNode("ambilight", "Ambilight", "WS2812B");
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Serial << endl
          << endl;
@@ -39,6 +41,7 @@ void setup() {
   Homie.setup();
 }
 
-void loop() {
+void loop()
+{
   Homie.loop();
 }
